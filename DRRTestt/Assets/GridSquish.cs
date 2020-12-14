7a8ae339a815d334f0eb0b95cb583bbb10ee342e
lpug21 <70877701+lpug21@users.noreply.github.com>
2020-12-15 09:29:33 +1100

    -Added DRRTest

---
+++ DRRTestt/Assets/GridSquish.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class GridSquish : MonoBehaviour
+{
+
+	public MeshFilter _0, _250, _500, _750, _1000, _1250;
+	Texture2D tex;
+	GameObject femurGroup;
+
+	void Start()
+    {
+		tex = new Texture2D(100, 100);
+		femurGroup = GameObject.Find("FemurGroup");
+		
+		DefineScreenGrid();
+    }
+
+
+	List<ScreenGridRow> ScreenGridPoints = new List<ScreenGridRow>();
+	private void DefineScreenGrid() {
+			
+		float x = tex.width;
+		float y = tex.height;
+		for (int i = 1; i < x + 1; i++) {
+			for (int j = 1; j < y + 1; j++) {
+				//Mid points for each grid
+				Vector3 midPoint = Camera.main.ViewportToWorldPoint(new Vector3(i / x, j / y, 0));
+
+				//if midpoint x/y is outside of bounds?  
+				if(Physics.Raycast(new Ray(midPoint, transform.forward))) {
+
+					ScreenGridRow s = new ScreenGridRow();
+					s.MidPoint = midPoint;
+					s.Testpoints = new Vector3[10];
+
+					//define test points?
+					for (int q = 0; q < 10; q++) {
+						s.Testpoints[q] = midPoint + new Vector3(0, 0, q + 1 * 10);
+					}
+					ScreenGridPoints.Add(s);
+				}
+
+			}
+		}
+
+		TestScreenGridPoints();
+	}
+
+	private void TestScreenGridPoints() {
+
+		//Assign each point an associated row										
+		Dictionary<Vector3, ScreenGridRow> pointGridLU = new Dictionary<Vector3, ScreenGridRow>(); //can use vector3 to LU associated row
+		foreach(ScreenGridRow s in ScreenGridPoints) {
+			foreach(Vector3 v in s.Testpoints) {
+				pointGridLU.Add(v, s);
+			}
+		}
+
+		//Construct array of test points for NN.
+		int i = 0;
+		Vector3[] points = new Vector3[pointGridLU.Count];
+		foreach(KeyValuePair<Vector3, ScreenGridRow> pair in pointGridLU) {
+			points[i] = pair.Key;
+			i++;
+		}
+
+		//Construct array of originalMesh points
+
+
+
+
+
+	}
+
+}
+
+class ScreenGridRow {
+	public Vector3 MidPoint;
+	public Vector3[] Testpoints;
+	public Color outputColour;
+}
+
+class MeshPointArray {
+	public Vector3[] Points;
+	public int Density;
+}
---
+++ DRRTestt/Assets/NearestNeighborInterface.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+using System;
+
+/// 
+/// This is a wrapper class that written to interface with Plugin I generated from this C++ Library: http://www.cs.umd.edu/~mount/ANN/
+/// It requires an array of points to compare against (and that array's length), the original points we're testing against (and that array's length)
+/// as well as an error bound, which determines the accuracy/speed of the result.  
+/// 
+/// |================================================================================================================================================|
+/// |Note:  This is assessing the 2D nearest neighbor, so the splayed components laid on top of each other in X/Y dimension, and assesssing the      |
+/// |nearest point in the compared component array.  This does not account for "Progression" - or the Z dimension of the PolarPoint.                 |
+/// |================================================================================================================================================|
+/// 
+
+
+public static class NearestNeighborInterface {
+
+#if UNITY_WEBGL && !UNITY_EDITOR
+    [DllImport("__Internal", EntryPoint = "GetNN")]
+#else
+    [DllImport("ANN", EntryPoint = "GetNN")]
+#endif
+    private static extern IntPtr GetNeighborArray(/*float[,]*/float[] comparedPoints, int comparedCount, /*float[,]*/float[] originalPoints, int originalCount, double errorBound);
+
+#if UNITY_WEBGL && !UNITY_EDITOR
+    [DllImport("__Internal")]
+#else
+    [DllImport("ANN", EntryPoint = "GetDists")]
+#endif
+    private static extern IntPtr GetDists();
+
+#if UNITY_WEBGL && !UNITY_EDITOR
+    [DllImport("__Internal")]
+#else
+    [DllImport("ANN", EntryPoint = "FreeMem")]
+#endif
+    public static extern IntPtr FreeMem(IntPtr ptr);
+
+#if UNITY_WEBGL && !UNITY_EDITOR
+    [DllImport("__Internal")]
+#else
+    [DllImport("ANN", EntryPoint = "FreeMemFloat")]
+#endif
+    public static extern IntPtr FreeMemFloat(IntPtr ptr);
+
+    //keep these the largest possible
+    static float[] pointsCompared;
+    static float[] pointsOrig;
+
+    /// <summary>
+    /// This returns an array of Nearest Neighbor Indices, ordered by the original point indices. 
+    /// That is, the returnedNN[i] is the index number in meshPointsCompared[] - which is the nearest neighbor to meshPointsOriginal[i] 
+    /// e.g.
+    ///  for (int i = 0; i < v3sOriginal.Length; i++){
+    ///     Vector3 NNPoint = v3sCompared[returnedNN[i]];
+    ///  }
+    /// </summary>
+    /// <param name="meshPointsCompared"></param>
+    /// <param name="meshPointsOriginal"></param>
+    /// <param name="returnedNNs"></param>
+    /// <param name="returnedDists"></param>
+    public static void GetNNsandDist(Vector3[] meshPointsCompared, Vector3[] meshPointsOriginal, int[] returnedNNs, float[] returnedDists) {
+        ////Compared Mesh points
+        //float[,] pointsCompared = new float[meshPointsCompared.Length, 2];
+        //for (int i = 0; i < meshPointsCompared.Length; i++) {
+        //    pointsCompared[i, 0] = meshPointsCompared[i].x;
+        //    pointsCompared[i, 1] = meshPointsCompared[i].y;
+        //}
+
+        ////original mesh points
+        //float[,] pointsOrig = new float[meshPointsOriginal.Length, 2];
+        //for (int i = 0; i < meshPointsOriginal.Length; i++) {
+        //    pointsOrig[i, 0] = meshPointsOriginal[i].x;
+        //    pointsOrig[i, 1] = meshPointsOriginal[i].y;
+
+        // }
+
+        //Compared Mesh points
+        if ((pointsCompared == null) || (pointsCompared.Length < meshPointsCompared.Length * 2)) {
+            pointsCompared = new float[meshPointsCompared.Length * 2];
+        }
+        for (int i = 0; i < meshPointsCompared.Length; i++) {
+            pointsCompared[2 * i] = meshPointsCompared[i].x;
+            pointsCompared[2 * i + 1] = meshPointsCompared[i].y;
+        }
+
+        //original mesh points
+        if ((pointsOrig == null) || (pointsOrig.Length < meshPointsOriginal.Length * 2)) {
+            pointsOrig = new float[meshPointsOriginal.Length * 2];
+        }
+        for (int i = 0; i < meshPointsOriginal.Length; i++) {
+            pointsOrig[2 * i] = meshPointsOriginal[i].x;
+            pointsOrig[2 * i + 1] = meshPointsOriginal[i].y;
+        }
+
+        double errorBound = 1000000;
+
+        IntPtr returnedIntPtr = GetNeighborArray(pointsCompared, meshPointsCompared.Length, pointsOrig, meshPointsOriginal.Length, errorBound);
+        ///returnedNNs = new int[meshPointsOriginal.Length];
+        Marshal.Copy(returnedIntPtr, returnedNNs, 0, meshPointsOriginal.Length);
+
+        IntPtr returnedFloatPtr = GetDists();
+        ///returnedDists = new float[meshPointsOriginal.Length];
+        Marshal.Copy(returnedFloatPtr, returnedDists, 0, meshPointsOriginal.Length);
+
+        //FreeMem(returnedIntPtr);
+        //FreeMemFloat(returnedFloatPtr);
+    }
+}
+
+
