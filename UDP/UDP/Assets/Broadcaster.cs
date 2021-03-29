3d1f597bb993db814240d567945b8f1c15284b42
lpug21 <dan@page2.com.au>
2021-03-29 16:12:32 +1100

    InitialUDB pushed happy daiz

---
+++ UDP/UDP/Assets/Broadcaster.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine;
+
+public class Broadcaster : MonoBehaviour {
+	private Socket s;
+	private IPEndPoint ep;
+	private IPAddress broadcast;
+
+	private void Start() {
+		s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+		broadcast = IPAddress.Parse("192.168.1.175");
+
+		ep = new IPEndPoint(broadcast, 11001);
+	}
+
+	// Update is called once per frame
+	private void Update() {
+		byte[] sendbuf = Encoding.ASCII.GetBytes("sending a message via udb babeyyy");
+
+		s.SendTo(sendbuf, ep);
+
+		Debug.Log("Message sent to the broadcast address " + DateTime.Now.ToString());
+	}
+}
